a376a1a54bea041035bc641637c7cfeb058baf45
four154 <155321989+four154@users.noreply.github.com>
2023-12-31 21:23:13 +0300

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,15 @@
+<div class="nosel d-flex room r187bfd6ab49xab6c6e61xmqzvs4 bord" style="text-align: left; width: 100%; border-bottom: 1px solid lavender; padding: 0px 0px 0px 1px; background-color: rgba(192, 112, 112, 0.024);" onclick="rjoin('187bfd6ab49xab6c6e61xmqzvs4');" v="1" n="« َخــَاصَ بالتصميم ♣ » ">
+        <div class="d-flex flex-grow-1" style="overflow: hidden;padding: 1px;">
+          <div style="min-width: 52px; width: 52px; min-height: 50px; max-height: 50px; background-color: rgb(243, 243, 243); background-image: url(&quot;pic/5CS1R.jpg.png&quot;);" class="fitimg u-pic borderg">
+          </div>"(https://up6.cc/2023/12/170404494487961.png)"
+          <div style="padding: 0px 2px;overflow: hidden;" class="flex-grow-1 break">
+            <div style="width:100%;display: flex;overflow: hidden;line-height: normal;margin-top:1px;" class="d-flex">
+              <div class="flex-grow-1"><span style="padding: 0px 4px; max-width: 100%; border-radius: 2px; color: rgb(222, 142, 142);" class="u-topic dots">« همــس وأحبابـها » </span></div>
+            </div>
+            <div style="color:#888;margin-top:-1px;" class="mini u-msg">تصاميمـ بنرات احترافية تصميم رومات</div>
+          </div>
+        </div>
+        <div class="c-flex">
+          <span class="corner fa fa-user label uc fa-microphone label-danger" style="padding:3px;margin:1px;border-radius: 2px;min-width: 60px;text-align: center;">1/20</span>
+        </div>
+      </div>
